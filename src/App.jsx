d1aa9cda22a6c0f5ee9c3bd6bae0f99edc8b22ec
SubashMCA23 <psubashh2003@gmail.com>
2026-08-16 11:14:51 +0530

    img

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,7 @@ import { Contact } from './pages/Contact';
 
 function App() {
   return (
-    <BrowserRouter>
+    <BrowserRouter basename={import.meta.env.BASE_URL}>
       <div className="flex flex-col min-h-screen">
         <Navbar />
         <main className="flex-grow">
